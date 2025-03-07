10648e510e30c4e37a5c7964225cb17171cc8542
Ebtisam.elnahas20@gmail.com <Ebtisam.elnahas20@gmail.com>
2025-03-07 14:43:41 +0200

    se

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 const connectDB=async()=>{
-    await mongoose.connect(process.env.mongoose_url).then(()=>{console.log("connect DB successfully");
+    await mongoose.connect(process.env.url_online).then(()=>{console.log("connect DB successfully");
     }).catch((error)=>{console.log(error.message , " "+ "error while connect to DB");
     })
 }
